cfc3412c4ed49dac531ebe0e883eb8eb578fc756
Arthur de Castro <arthur_ravin@hotmail.com>
2019-03-21 21:58:32 -0300

    Enumeration screens created

---
+++ Movies/Coordinator/Display.swift
@@ -0,0 +1,26 @@
+//
+//  Display.swift
+//  Movies
+//
+//  Created by Arthur Castro on 21/03/19.
+//  Copyright © 2019 Arthur Castro. All rights reserved.
+//
+
+import Foundation
+
+
+struct Display {
+    ///Reference controller
+    let controller : String
+    ///Reference storyboard
+    let storyboard: String
+    
+    
+    
+    ///Add Calling Screens
+    static let displayMovies = Display(controller: "DisplayMoviesViewController", storyboard: "DisplayMovies")
+    
+    
+    
+    
+}
