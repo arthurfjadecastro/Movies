b49e142ae73a75a922db1e43106c417ce6cb206e
Arthur de Castro <arthur_ravin@hotmail.com>
2019-03-21 21:16:36 -0300

    Initial Commit

---
+++ Movies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Arthur Castro on 21/03/19.
+//  Copyright © 2019 Arthur Castro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
