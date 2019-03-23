020615a000284528b5f4ad30437a2b0a94424758
Arthur de Castro <arthur_ravin@hotmail.com>
2019-03-23 01:45:21 -0300

    CollectionView layout is fluid

--- Movies/DisplayMoviesViewController.swift
+++ Movies/DisplayMoviesViewController.swift
@@ -9,8 +9,7 @@
 import UIKit
 
 
-
-
+fileprivate let kScale: CGFloat = 0.6
 
 class DisplayMoviesViewController: UIViewController, Coordinable {
     
@@ -34,8 +33,20 @@ class DisplayMoviesViewController: UIViewController, Coordinable {
     
     
     func setupCollectionView(){
+        let _screenSize = UIScreen.main.bounds.size
+        let _cellWidth = floor(_screenSize.width * kScale)
+        let _cellHeight = floor(_screenSize.height * kScale)
+        let _insetX = (self.view.bounds.width - _cellWidth) / 2
+        let _insetY = (self.view.bounds.height - _cellHeight) / 2
+        
+        let _layout = self.moviesCollectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        
+        _layout.itemSize = CGSize(width: _cellWidth, height: _cellHeight)
+        
+        self.moviesCollectionView.contentInset = UIEdgeInsets(top: _insetY, left: _insetX, bottom: _insetY, right: _insetX)
+        
         self.moviesCollectionView.dataSource = self
-//        self.moviesCollectionView.delegate = self
+        self.moviesCollectionView.delegate = self
     }
     
  
@@ -65,8 +76,38 @@ extension DisplayMoviesViewController: UICollectionViewDataSource {
     
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1 
+        return 1
     }
+    
+    
+    
+    
+}
+
+
+extension DisplayMoviesViewController: UIScrollViewDelegate {
+    
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        let _layout = self.moviesCollectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        
+        let _cellWidthSpacing = _layout.itemSize.width + _layout.minimumLineSpacing
+        
+        var _offset = targetContentOffset.pointee
+        
+        let _index = (_offset.x + scrollView.contentInset.left) / _cellWidthSpacing
+        
+        let _roundedIndex = round(_index)
+        
+        _offset = CGPoint(x: _roundedIndex * _cellWidthSpacing - scrollView.contentInset.left, y: scrollView.contentInset.top)
+        
+        targetContentOffset.pointee = _offset
+    }
+    
+    
+}
+
+
+extension DisplayMoviesViewController: UICollectionViewDelegate {
     
     
     
--- Movies/MoviesConcern.swift
+++ Movies/MoviesConcern.swift
@@ -31,8 +31,10 @@ class MoviesConcern {
     
     static func fetchMovies() -> [MoviesConcern] {
         return [
-            MoviesConcern(title: "ae1", image:#imageLiteral(resourceName: "ae1.png") , duration: "10 hours", genre: "Fe", synopsis: "MTO LOUCO O FILME"),
-            MoviesConcern(title: "ae12", image:#imageLiteral(resourceName: "ae2.png") , duration: "5 hours", genre: "MA", synopsis: "MTO LOUCO O FILME2")
+            MoviesConcern(title: "ae1", image:#imageLiteral(resourceName: "movie01.jpeg") , duration: "10 hours", genre: "Fe", synopsis: "MTO LOUCO O FILME"),
+            MoviesConcern(title: "ae12", image:#imageLiteral(resourceName: "movie02.jpeg") , duration: "5 hours", genre: "MA", synopsis: "MTO LOUCO O FILME2"),
+            MoviesConcern(title: "ae12", image:#imageLiteral(resourceName: "movie03.png") , duration: "5 hours", genre: "MA", synopsis: "MTO LOUCO O FILME2"),
+            MoviesConcern(title: "ae12", image: #imageLiteral(resourceName: "movie04.png"), duration: "5 hours", genre: "MA", synopsis: "MTO LOUCO O FILME2")
             
         ]
     }
