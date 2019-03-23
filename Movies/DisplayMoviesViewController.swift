dc50dd6cba78a4616eb4c8e2c4a389b89d6749cc
Arthur de Castro <arthur_ravin@hotmail.com>
2019-03-23 01:17:44 -0300

    collectionview was added and populated with mockdata

--- Movies/DisplayMoviesViewController.swift
+++ Movies/DisplayMoviesViewController.swift
@@ -14,16 +14,61 @@ import UIKit
 
 class DisplayMoviesViewController: UIViewController, Coordinable {
     
+    //MARK: - Properties
     
     var coordinator: Coordinator?
+    var movies = MoviesConcern.fetchMovies()
     
+    //MARK: - IBO
+    
+    @IBOutlet weak var moviesCollectionView: UICollectionView!
 
+    
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.setupCollectionView()
+        
+        
     }
     
+    
+    func setupCollectionView(){
+        self.moviesCollectionView.dataSource = self
+//        self.moviesCollectionView.delegate = self
+    }
+    
+ 
+
+    
+}
 
 
+
+//MARK: - UICollectionViewDataSource
+
+extension DisplayMoviesViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.movies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let _cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MoviesCollectionViewCell", for: indexPath) as! MoviesCollectionViewCell
+        
+        let _movie = self.movies[indexPath.item]
+        
+        _cell.movie = _movie
+        
+        return _cell
+        
+    }
+    
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1 
+    }
+    
+    
+    
     
 }
